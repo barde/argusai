1a49331979e59a792dc3147f546fb51084d27129
Bartholomäus Dedersen <git@selbstapotheose.de>
2025-06-26 22:09:20 +0200

    Test webhook with correct secret

--- test/large-file-test.js
+++ test/large-file-test.js
@@ -1,7 +1,7 @@
 /**
  * Large File Test for ArgusAI Chunking
  * This file contains multiple issues to ensure the bot can review large files
- * Updated: Testing chunking implementation v2
+ * Updated: Testing chunking implementation v3 - webhook secret fixed
  */
 
 // Section 1: Authentication Issues
